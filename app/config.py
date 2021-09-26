83bb8b3c10b588b94e715c33ae87d8ae11b044dd
Ramil Gadzhiev <rami.gaggi@gmail.com>
2021-09-26 18:36:44 +0300

    fix: bug with empty recipients

--- app/config.py
+++ app/config.py
@@ -20,4 +20,4 @@ class Config():
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    RECIPIENTS = os.environ.get('RECIPIENTS').split(',')
+    RECIPIENTS = os.environ.get('RECIPIENTS', ',').split(',')
